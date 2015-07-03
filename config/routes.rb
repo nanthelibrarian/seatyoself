c2e94a0f4a4f9b68958fb3e48d8c25f4c91b11f4
AngeLopez <angel@angelopez.ca>
2015-07-03 15:44:57 -0400

    Restaurant controler and associations added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/new'
+
   resources :sessions, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
