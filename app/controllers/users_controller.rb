e98cb7722bd6cba99ce1700411783f1a559249c6
RHEA GANAPATHY <rheasg@gmail.com>
2015-07-03 17:04:17 -0400

    Minor changes to New users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_index_path, notice: "Signed up!"
+      redirect_to pages_homepage_path, notice: "Signed up!"
     else
       render "new"
   end
